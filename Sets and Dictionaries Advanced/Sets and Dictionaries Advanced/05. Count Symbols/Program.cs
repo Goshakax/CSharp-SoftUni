17e3194ed7bae69b6c7e3a0e15003fef696894b8
Goshakax <georgipetev07@gmail.com>
2023-02-03 13:27:46 +0200

    Added Sets And Dictionaries Advanced Exercises

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced/05. Count Symbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<char, int>();
+            string input = Console.ReadLine();
+
+            foreach (char currChar in input)
+            {
+                if (!dict.ContainsKey(currChar))
+                {
+                    dict.Add(currChar, 0);
+                }
+
+                dict[currChar]++;
+            }
+
+            dict = dict.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var currChar in dict)
+            {
+                Console.WriteLine($"{currChar.Key}: {currChar.Value} time/s");
+            }
+
+        }
+    }
+}
