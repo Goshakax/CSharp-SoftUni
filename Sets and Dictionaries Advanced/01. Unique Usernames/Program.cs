2f3e6be0c6539f423241a4fde68c34c1a4d0502e
Goshakax <georgipetev07@gmail.com>
2023-02-03 14:01:16 +0200

    Added Sets And Dictionaries Advanced Exercises

---
+++ Sets and Dictionaries Advanced/01. Unique Usernames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace сет
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+
+                set.Add(name);
+            }
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced/04. Even Times/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<int, int>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(num))
+                {
+                    dict.Add(num, 0);
+                }
+
+                dict[num]++;
+            }
+
+            foreach (var item in dict)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
