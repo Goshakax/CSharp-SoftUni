16dd6182be1691453d4c52e3d14ae1bcfb05a4c3
Goshakax <georgipetev07@gmail.com>
2023-02-03 13:47:35 +0200

    Added Streams, Fukes and Directiories Exercises

---
+++ Streams, Files and Directories/01. Even Lines/EvenLines.cs
@@ -0,0 +1,58 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Xml;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using (StreamReader r = new StreamReader(inputFilePath))
+            {
+                string line = string.Empty;
+                int lineNum = 0;
+                string output = null;
+
+                while ((line = r.ReadLine()) != null)
+                {
+                    if (lineNum % 2 == 0)
+                    {
+                         line.Reverse();
+
+                        line = line.Replace(',', '@');
+                        line = line.Replace('-', '@');
+                        line = line.Replace('!', '@');
+                        line = line.Replace('?', '@');
+                        line = line.Replace('.', '@');
+
+                        string[] reversedLine = line.Split(' ').Reverse().ToArray();
+
+                        if (lineNum == 0)
+                        {
+                            output += $"{string.Join(' ', reversedLine)}";
+                        }
+                        else
+                        {
+                            output += $"\n{string.Join(' ', reversedLine)}";
+                        }
+                        
+                    }
+                    lineNum++;
+                }
+
+                return output;
+            }
+
+            
+        }
+    }
+}
---
+++ Streams, Files and Directories/04. Directory Traversal/DirectoryTraversal.cs
@@ -0,0 +1,72 @@
+﻿namespace DirectoryTraversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net.Sockets;
+    using System.Text.RegularExpressions;
+
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
+        }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(inputFolderPath);
+
+            FileInfo[] fileInfos = dir.GetFiles("*", SearchOption.AllDirectories);
+            FileInfo[] infos = fileInfos;
+            string output = string.Empty;
+            Dictionary<string, Dictionary<string, long>> dict = new Dictionary<string, Dictionary<string, long>>();
+
+            foreach (FileInfo file in infos) 
+            {
+                if (!dict.ContainsKey(file.Extension))
+                {
+                    dict.Add(file.Extension, new Dictionary<string, long>());
+                }
+
+                dict[file.Extension].Add(file.Name, file.Length); 
+            }
+            int count = 0;
+            foreach (var extension in dict) 
+            {
+                if (count == 0)
+                {
+                    output += extension.Key;
+                }
+                else
+                {
+                    output += $"\n{extension.Key}";
+                }
+
+                foreach (var file in extension.Value)
+                {
+                    output += $"\n--{file.Key} - {file.Value}kb";
+                }
+
+                count++;
+            }
+
+           return output;
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            using (StreamWriter w = new StreamWriter($"{desktopPath}{reportFileName}"))
+            {
+                w.Write(textContent);
+            }
+        }
+    }
+}
